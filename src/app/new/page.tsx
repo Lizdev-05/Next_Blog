3c7b6bd42897db17526084f8f6e52335a1de6ece
Ojesanmi Elizabeth <ojesanmioyinlade@gmail.com>
2023-09-07 20:05:28 +0100

    Create the form for new blog

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,7 +1,36 @@
+import Link from "next/link";
 import React from "react";
 
 const New = () => {
-  return <div>New</div>;
+  return (
+    <>
+      <header className="flex justify-between items-center mb-4">
+        <h1 className="text-2xl">New</h1>
+      </header>
+
+      <form className="flex gap-2 flex-col">
+        <input
+          type="text"
+          name="title"
+          className="border  bg-transparent rounded px-2 py-1 outline-none focus-within:border-slate-300"
+        />
+
+        <div className="flex gap-1 justify-end">
+          <Link
+            href=".."
+            className=" border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none"
+          >
+            {" "}
+            Back
+          </Link>
+          <button className="border border-slate-300 text-slate-300 px-2 py-1 rounded hover:bg-slate-700 focus-within:bg-slate-700 outline-none">
+            {" "}
+            Submit
+          </button>
+        </div>
+      </form>
+    </>
+  );
 };
 
 export default New;
--- src/components/TodoItem.tsx
+++ src/components/TodoItem.tsx
@@ -9,7 +9,10 @@ const TodoItem = ({ id, title, complete }: TodoItemsProps) => {
   return (
     <li className="flex gap-1 items-center">
       <input type="checkbox" id={id} className="cursor-pointer peer" />
-      <label htmlFor={id} className="peer-checked:line-through">
+      <label
+        htmlFor={id}
+        className="peer-checked:line-through cursor-pointer peer-checked:text-slate-500"
+      >
         {title}
       </label>
     </li>
